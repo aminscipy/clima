b51ee1c06a70a76c3202adc92406362d30d924a2
amin <thisisaamin@gmail.com>
2022-09-18 15:01:58 +0530

    try catch added for location

--- lib/services/location.dart
+++ lib/services/location.dart
@@ -2,28 +2,32 @@ import 'package:geolocator/geolocator.dart';
 
 class Location {
   Future determinePosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
+    try {
+      bool serviceEnabled;
+      LocationPermission permission;
 
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      return Future.error('Location services are disabled.');
-    }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        return Future.error('Location permissions are denied');
+      serviceEnabled = await Geolocator.isLocationServiceEnabled();
+      if (!serviceEnabled) {
+        return Future.error('Location services are disabled.');
       }
-    }
 
-    if (permission == LocationPermission.deniedForever) {
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
+      permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied) {
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied) {
+          return Future.error('Location permissions are denied');
+        }
+      }
+
+      if (permission == LocationPermission.deniedForever) {
+        return Future.error(
+            'Location permissions are permanently denied, we cannot request permissions.');
+      }
+      var currentLocation = await Geolocator.getCurrentPosition();
+      print('${currentLocation.latitude}, ${currentLocation.longitude}');
+    } catch (e) {
+      print(e);
     }
-    var currentLocation = await Geolocator.getCurrentPosition();
-    print('${currentLocation.latitude}, ${currentLocation.longitude}');
   }
 }
 
